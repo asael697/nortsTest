d31438c8289422a1b5665593e6c12aba3b824390
Asael A Matamoros <asael_am@hotmail.com>
2023-12-24 10:52:06 -0600

    Add Lobato and Velasco's sieve-bootstrap test

--- R/lobato_test.R
+++ R/lobato_test.R
@@ -148,3 +148,87 @@ lobato.statistic = function(y,c = 1){
 
   return(G)
 }
+
+#' The Sieve Bootstrap Lobato and Velasco's Test for normality.
+#'
+#' Performs the Lobato and Velasco's test for normality approximating the linear
+#' time series using a sieve Bootstrap procedure. The null hypothesis (H0),
+#' is that the given data follows a Gaussian process.
+#'
+#' @usage lobato_bootstrap.test(y, c = 1, reps = 1000, h = 100, seed = NULL)
+#'
+#' @param y a numeric vector or an object of the \code{ts} class containing a stationary
+#' time series.
+#' @param c a positive real value that identifies the total amount of values used in the
+#' cumulative sum.
+#' @param reps an integer with the total bootstrap repetitions.
+#' @param reps an integer with the total bootstrap repetitions.
+#' @param h an integer with the first \code{burn-in} sieve bootstrap replicates.
+#' @param seed An optional \code{\link[=set.seed]{seed}} to use.
+#'
+#' @return A list with class \code{"h.test"} containing the following components:
+#' \itemize{
+#'  \item{statistic }{the sieve bootstrap Lobato  n Velasco's statistic.}
+#'  \item{p.value }{the p value for the test.}
+#'  \item{alternative }{a character string describing the alternative hypothesis.}
+#'  \item{method }{a character string \dQuote{Sieve-Bootstrap Lobato's test}.}
+#'  \item{data.name }{a character string giving the name of the data.}
+#' }
+#'
+#' @details
+#' This test proves a normality assumption in correlated data employing the
+#' skewness-kurtosis test statistic proposed by \emph{Lobato, I., & Velasco, C. (2004)},
+#' approximating the p-value using a sieve-bootstrap procedure,
+#' \emph{Psaradakis, Z. and Vávra, M. (2020)}.
+#'
+#' @export
+#'
+#' @author Asael Alonzo Matamoros and Alicia Nieto-Reyes.
+#'
+#' @seealso \code{\link{lobato.statistic}},\code{\link{epps.test}}
+#'
+#' @references
+#' Psaradakis, Z. and Vávra, M. (2020) Normality tests for dependent
+#' data: large-sample and bootstrap approaches. Communications in
+#' \emph{Statistics-Simulation and Computation 49 (2)}. ISSN 0361-0918.
+#'
+#' Nieto-Reyes, A., Cuesta-Albertos, J. & Gamboa, F. (2014). A random-projection
+#' based test of Gaussianity for stationary processes. \emph{Computational
+#' Statistics & Data Analysis, Elsevier}, vol. 75(C), pages 124-141.
+#'
+#' Lobato, I., & Velasco, C. (2004). A simple test of normality in time series.
+#' \emph{Journal of econometric theory}. 20(4), 671-689.
+#'
+#' @examples
+#' # Generating an stationary arma process
+#' y = arima.sim(1000,model = list(ar = 0.3))
+#' lobato_bootstrap.test(y, reps = 1000)
+#'
+lobato_bootstrap.test = function(y, c = 1, reps = 1000, h = 100, seed = NULL){
+
+  if( !is.numeric(y) & !is(y,class2 = "ts") )
+    stop("y object must be numeric or a time series")
+
+  if( anyNA(y) )
+    stop("The time series contains missing values")
+
+  # checking stationarity
+  cc = uroot.test(y)
+  if(!cc$stationary)
+    warning("y has a unit root, vavra.test requires stationary process")
+
+  # checking seasonality
+  if(frequency(y) > 1){
+    cc = seasonal.test(y)
+    if(cc$seasonal)
+      warning("y has a seasonal unit root, vavra.test requires stationary process")
+  }
+
+  if (!is.null(seed))
+    set.seed(seed)
+
+  htest = vavra.test(y = y, c = c, normality = "lobato",
+                     reps = reps, h = h, seed = seed)
+
+  return(htest)
+}
