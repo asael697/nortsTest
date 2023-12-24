513ecb639385d5241ae8cde73ac61706d0ec6bc5
Asael A Matamoros <asael_am@hotmail.com>
2023-12-24 11:27:42 -0600

    add unit tests for the jb_bootstrap.test

--- tests/testthat/test-basics.R
+++ tests/testthat/test-basics.R
@@ -3,11 +3,21 @@
 ################################################################
 
 set.seed(169721)
-y = arima.sim(n = 600,model = list(ar = 0.3,ma = 0.2))
+y = arima.sim(n = 600, model = list(ar = 0.3, ma = 0.2))
 
 test_that("Vavra's test works for GPs", {
   start = Sys.time()
   ht = nortsTest::vavra.test(y)
+  end = Sys.time() - start
+  # check the test choose the right hypothesis when using a Gaussian ARMA
+  expect_equal(unname(ht$p.value >= 0.05), TRUE)
+  #check computations are less than 2.5s
+  expect_equal(end < 2.5, TRUE)
+})
+
+test_that("Bootstrap Jarque Bera test works for GPs", {
+  start = Sys.time()
+  ht = nortsTest::jb_bootstrap.test(y)
   end = Sys.time() - start
   # check the test choose the right hypothesis when using a Gaussian ARMA
   expect_equal(unname(ht$p.value >= 0.05), TRUE)
@@ -25,9 +35,29 @@ test_that("Lobato's test works for GPs", {
   expect_equal(end < 2.5, TRUE)
 })
 
+test_that("Bootstrap Lobato's test works for GPs", {
+  start = Sys.time()
+  ht = nortsTest::lobato_bootstrap.test(y)
+  end = Sys.time() - start
+  # check the test choose the right hypothesis when using a Gaussian ARMA
+  expect_equal(unname(ht$p.value >= 0.05), TRUE)
+  #check computations are less than 2.5s
+  expect_equal(end < 2.5, TRUE)
+})
+
 test_that("Epps' test works for GPs", {
   start = Sys.time()
   ht = nortsTest::epps.test(y)
+  end = Sys.time() - start
+  # check the test choose the right hypothesis when using a Gaussian ARMA
+  expect_equal(unname(ht$p.value >= 0.05), TRUE)
+  #check computations are less than 2.5s
+  expect_equal(end < 2.5, TRUE)
+})
+
+test_that("Bootstrap Epps' test works for GPs", {
+  start = Sys.time()
+  ht = nortsTest::epps_bootstrap.test(y)
   end = Sys.time() - start
   # check the test choose the right hypothesis when using a Gaussian ARMA
   expect_equal(unname(ht$p.value >= 0.05), TRUE)
@@ -50,11 +80,22 @@ test_that("Random Projections' test works for GPs", {
 ################################################################
 
 set.seed(169721)
-y = arima.sim(n = 600,model = list(ar = 0.3,ma = 0.2),rand.gen = rgamma,shape = 0.4)
+y = arima.sim(n = 600, model = list(ar = 0.3, ma = 0.2),
+              rand.gen = rgamma, shape = 0.4)
 
 test_that("Vavra's test works for non GPs", {
   start = Sys.time()
   ht = nortsTest::vavra.test(y)
+  end = Sys.time() - start
+  # check the test choose the right hypothesis when using a gamma ARMA
+  expect_equal(unname(ht$p.value < 0.05), TRUE)
+  #check computations are less than 2.5s
+  expect_equal(end < 2.5, TRUE)
+})
+
+test_that("Bootstrap Jarque Beras' test works for non GPs", {
+  start = Sys.time()
+  ht = nortsTest::jb_bootstrap.test(y)
   end = Sys.time() - start
   # check the test choose the right hypothesis when using a gamma ARMA
   expect_equal(unname(ht$p.value < 0.05), TRUE)
@@ -72,9 +113,29 @@ test_that("Lobato's test works for non GPs", {
   expect_equal(end < 2.5, TRUE)
 })
 
+test_that("Bootstrap Lobato's test works for non GPs", {
+  start = Sys.time()
+  ht = nortsTest::lobato_bootstrap.test(y)
+  end = Sys.time() - start
+  # check the test choose the right hypothesis when using a gamma ARMA
+  expect_equal(unname(ht$p.value < 0.05), TRUE)
+  #check computations are less than 2.5s
+  expect_equal(end < 2.5, TRUE)
+})
+
 test_that("Epps' test works for non GPs", {
   start = Sys.time()
   ht = nortsTest::epps.test(y)
+  end = Sys.time() - start
+  # check the test choose the right hypothesis when using a gamma ARMA
+  expect_equal(unname(ht$p.value < 0.05), TRUE)
+  #check computations are less than 2.5s
+  expect_equal(end < 2.5, TRUE)
+})
+
+test_that("Bootstrap Epps' test works for non GPs", {
+  start = Sys.time()
+  ht = nortsTest::epps_bootstrap.test(y)
   end = Sys.time() - start
   # check the test choose the right hypothesis when using a gamma ARMA
   expect_equal(unname(ht$p.value < 0.05), TRUE)
--- tests/testthat/test-performance.R
+++ tests/testthat/test-performance.R
@@ -3,11 +3,18 @@
 ################################################################
 
 set.seed(1697231)
-y = arima.sim(n = 3000,model = list(ar = 0.3,ma = 0.2))
+y = arima.sim(n = 3000, model = list(ar = 0.3, ma = 0.2))
 
 test_that("Vavra's test performance lower than 10s", {
   start = Sys.time()
   ht = nortsTest::vavra.test(y)
+  end = Sys.time() - start
+  expect_equal(end < 10, TRUE)
+})
+
+test_that("Bootstrap Jarque Beras' test performance lower than 10s", {
+  start = Sys.time()
+  ht = nortsTest::jb_bootstrap.test(y)
   end = Sys.time() - start
   expect_equal(end < 10, TRUE)
 })
@@ -19,9 +26,23 @@ test_that("Lobato's test performance lower than 10s", {
   expect_equal(end < 10, TRUE)
 })
 
+test_that("Bootstrap Lobato's test performance lower than 10s", {
+  start = Sys.time()
+  ht = nortsTest::lobato_bootstrap.test(y)
+  end = Sys.time() - start
+  expect_equal(end < 10, TRUE)
+})
+
 test_that("Epps' test performance lower than 10s", {
   start = Sys.time()
   ht = nortsTest::epps.test(y)
+  end = Sys.time() - start
+  expect_equal(end < 10, TRUE)
+})
+
+test_that("Bootstrap Epps' test performance lower than 10s", {
+  start = Sys.time()
+  ht = nortsTest::epps_bootstrap.test(y)
   end = Sys.time() - start
   expect_equal(end < 10, TRUE)
 })
